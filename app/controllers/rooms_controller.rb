784c9bfc0d25a348319b40ac93e611310f316c83
EC2 Default User <ec2-user@ip-172-31-33-188.us-east-2.compute.internal>
2022-04-11 11:23:42 +0000

    validetion

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,16 @@
 class RoomsController < ApplicationController
+    
+ def new
+     @room = Room.new
+ end
+ 
+ 
+ 
+ 
+ private 
+  def room_params
+        params.require(:room).permit(:game_id, :room_member_id, :member_max, :member_count, :owner_id, 
+                                     :image, :platform, :playstyle, :weekday, :play_timing, :introduction,:approval)
+  end
+    
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,22 @@
 class Room < ApplicationRecord
+  has_many :game_id
+  
   enum platform: Settings.platforms.to_h,_prefix: true
   enum playstyle: Settings.playstyles.to_h,_prefix: true
   enum weekday: Settings.weekdays.to_h,_prefix: true
   enum play_timing: Settings.play_timings.to_h,_prefix: true
+  
+  
+  with_options presence: true do
+    validates :game_id
+    validates :member_max,less_than_or_equal_to: 10
+    validates :owner_id
+    validates :platform
+    validates :playstyle
+    validates :playstyle
+    validates :play_timing
+  end
+    validates :introduction,length: { maximum: 100 } 
+    
+    attachment :image
 end
