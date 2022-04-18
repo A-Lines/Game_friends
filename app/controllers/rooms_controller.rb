6ff2ef924189cb298d01e3b2668d2e0ef5834730
EC2 Default User <ec2-user@ip-172-31-33-188.us-east-2.compute.internal>
2022-04-18 09:16:56 +0000

    view編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,8 +9,7 @@ class RoomsController < ApplicationController
     # if user_signed_in?
     # byebug
       if @room.save!
-        room_member = @room.room_members.create(user_id: 1,room_id: @room)
-        # ↑仮！！！！！！！ログイン機能がmergeされたら「current_user」に変更！！
+        room_member = @room.room_members.create(user_id: current_user,room_id: @room)
         room_talk_space = @room.room_talk_space.create(room_id: @room)
         redirect_to room_path(@room), success: "ルームを作成しました！"
       else
@@ -24,7 +23,8 @@ class RoomsController < ApplicationController
  
   def join_request
     @room = Room.find(params[:id])
-    @romm_owner = Roommember.where(user_id: owner_id).nickname
+    @romm_owner = RoomMember.where(user_id: @room.owner_id)
+    # User,Gameとアソシエーションしないと表示できない！！！！！
     @room_platforms = @room.i18n_true_platform_index
     @room_playstyles = @room.i18n_true_playstyle_index
     @room_weekdays = @room.i18n_true_weekday_index
@@ -37,7 +37,7 @@ class RoomsController < ApplicationController
                                     :platform_pc,:platform_playstation,:platform_xbox,:platform_switch,:platform_smartfone,
                                     :playstyle_enjoy,:playstyle_serious,:playstyle_beginner,
                                     :weekday_weekday,:weekday_holiday,:weekday_norule,
-                                    :timing_morning,:timing_afternoon,:timing_midnight,:timing_norule).merge(owner_id: 1)
+                                    :timing_morning,:timing_afternoon,:timing_midnight,:timing_norule).merge(owner_id: current_user)
   end
     
 end
